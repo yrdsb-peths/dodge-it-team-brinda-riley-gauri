b48da828c14203b713e69e4177a1705e6cab6144
gauri-n08 <gaurisknair08@gmail.com>
2025-04-24 08:42:02 -0400

    Updated ball to move

--- Ball.java
+++ Ball.java
@@ -1,19 +1,23 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Ball here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Ball extends Actor
 {
-    /**
-     * Act - do whatever the Ball wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
-        // Add your action code here.
+        move(-10);
+        
+        if (getX() <= 0) {
+            resetBall();
+        }
+    }
+    
+    public void resetBall()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600, 100); 
+        } else {
+            setLocation(600, 300);
+        }
     }
 }
